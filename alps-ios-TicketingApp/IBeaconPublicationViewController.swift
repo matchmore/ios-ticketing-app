a4b6442952c19af76f2185301c244681968548e8
Wen <jmd@wl2s-237-13.unil.ch>
2017-09-14 10:49:55 +0200

    Working version with portal and beacon

--- alps-ios-TicketingApp/IBeaconPublicationViewController.swift
+++ alps-ios-TicketingApp/IBeaconPublicationViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 import AlpsSDK
+import Alps
 
-class IBeaconPublicationViewController: UIViewController, UITextFieldDelegate {
+class IBeaconPublicationViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var concertTextField: UITextField!
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var imageTextField: UITextField!
     @IBOutlet weak var durationTextField: UITextField!
-    @IBOutlet weak var proximityUUIDTextField: UITextField!
-    @IBOutlet weak var majorTextField: UITextField!
-    @IBOutlet weak var minorTextField: UITextField!
+    @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var publishButton: UIButton!
     var i = 1
+    var pickerData : [IBeaconDevice] = []
+    var selectedValue : IBeaconDevice?
     
     // Using appDelegate as a singleton
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -27,18 +28,18 @@ class IBeaconPublicationViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Connect data:
+        self.picker.delegate = self
+        self.picker.dataSource = self
         alps = self.appDelegate.alps
         self.concertTextField.delegate = self
         self.priceTextField.delegate = self
         self.imageTextField.delegate = self
         self.durationTextField.delegate = self
-        self.proximityUUIDTextField.delegate = self
-        self.majorTextField.delegate = self
-        self.minorTextField.delegate = self
         self.priceTextField.keyboardType = .numbersAndPunctuation
         self.durationTextField.keyboardType = .numbersAndPunctuation
-        self.majorTextField.keyboardType = .numbersAndPunctuation
-        self.minorTextField.keyboardType = .numbersAndPunctuation
+        self.pickerData = self.appDelegate.alps.getBeacons()
+        self.selectedValue = pickerData[0]
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -50,11 +51,31 @@ class IBeaconPublicationViewController: UIViewController, UITextFieldDelegate {
         return false
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row].name
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        self.selectedValue = pickerData[row]
+    }
+    
     @IBAction func publishAction(_ sender: Any) {
         publishButton.isEnabled = false
         
-        if let price = Double(priceTextField.text!), let image = imageTextField.text, let concert = concertTextField.text, let duration = Double(durationTextField.text!), let proximityUUID = proximityUUIDTextField.text, let major = Double(majorTextField.text!), let minor = Double(minorTextField.text!){
-            createPublication(concert: concert, price: price, image: image, duration: duration, proximityUUID: proximityUUID, major: major, minor: minor, completion: {
+        if let price = Double(priceTextField.text!), let image = imageTextField.text, let concert = concertTextField.text, let duration = Double(durationTextField.text!), let deviceId = self.selectedValue?.id {
+            createPublication(concert: concert, price: price, image: image, duration: duration, deviceId: deviceId, completion: {
                 () in
                 self.navigationController?.popToRootViewController(animated: true)
             })
@@ -73,11 +94,7 @@ class IBeaconPublicationViewController: UIViewController, UITextFieldDelegate {
     }
 
     // MARK: - AlpsSDK
-    func createPublication(concert: String, price: Double, image: String, duration: Double, proximityUUID: String, major: Double, minor: Double, completion: @escaping () -> Void) {
-        let realMajor = NSNumber.init(value: major)
-        let realMinor = NSNumber.init(value: minor)
-        self.appDelegate.alps.createIBeaconDevice(name: "beacon device \(i)", proximityUUID: proximityUUID, major: realMajor, minor: realMinor, completion: {
-            (_ device) in
+    func createPublication(concert: String, price: Double, image: String, duration: Double, deviceId : String, completion: @escaping () -> Void) {
             print("IBeacon DEVICE Created")
             // XXX: the property syntax is tricky at the moment: mood is a variable and 'happy' is a string value
             var properties : [String:String] = [:]
@@ -85,19 +102,16 @@ class IBeaconPublicationViewController: UIViewController, UITextFieldDelegate {
             properties["price"] = "\(price)"
             properties["image"] = image
             properties["deviceType"] = "iBeacon"
-            if let deviceId = device?.id{
                 self.alps.createPublication(userId: self.appDelegate.userId!, deviceId: deviceId, topic: "ticketstosale", range: 0.0, duration: duration, properties: properties, completion: {
                     (_ publication) in
                     if let p = publication {
                         print("DEVICE ID : ")
-                        print(device?.id)
+                        print(deviceId)
                         print("Created publication: id = \(String(describing: p.id)), topic = \(String(describing: p.topic)), properties = \(String(describing: p.properties))")
                         self.i += 1
 //                        self.appDelegate.alps.addBeacon(beacon: device!)
                         completion()
                     }
                 })
-            }
-        })
     }
 }
